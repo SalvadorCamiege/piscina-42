5ca35c1b356ae9a3e0904401d7584f28419b963e
Salvador Camiege <camiegesalvador@gmail.com>
2024-07-21 22:09:44 +0100

    ft_reverse

--- ex02/ft_print_reverse_alphabet.c
+++ ex02/ft_print_reverse_alphabet.c
@@ -1,18 +1,9 @@
 #include <unistd.h>
 
-int main(void){
-    ft_print_reverse_alphabet();
-
-
-}
-
-
 void ft_print_reverse_alphabet(void){
       char letra = 'z';
            while(letra >= 'a'){
                 write(1,&letra,1);
                     letra--;
-
            }
-
 }
