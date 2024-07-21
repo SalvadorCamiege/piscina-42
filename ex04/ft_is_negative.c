24a3aeff06b881b3a5aae3713d510717f5d8e63c
Salvador Camiege <camiegesalvador@gmail.com>
2024-07-21 23:23:57 +0100

    add ft_is_negative.c

---
+++ ex04/ft_is_negative.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+
+int main(){
+   ft_is_negative();    
+
+}
+
+
+void ft_is_negative(int n){
+     char resp;
+     if(n <= '0'){
+          resp = 'P';
+             write(1,&resp,1);
+
+     }else{
+          resp = 'N';
+          write(1,&resp,1);
+     }
+
+}
